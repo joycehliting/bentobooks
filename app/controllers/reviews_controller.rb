cf48fe2d8e70eae66d4ac14949729ca13cda0298
joycehliting <joycehliting@gmail.com>
2015-02-01 20:33:09 +0800

    includes Authorization

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only [:edit, :update, :destroy]
 
   # GET /reviews
   # GET /reviews.json
@@ -11,37 +13,58 @@ class ReviewsController < ApplicationController
   end
 
   def new
-    @review = Review.new
+    @review = current_user.reviews.build
   end
 
   def edit
   end
 
   def create
-    @review = Review.new(review_params)
-
-    respond_to do |format|
+    @review = current_user.reviews.build(review_params)
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
+       redirect_to @review, notice: 'Review was successfully created.' 
 
       else
-        format.html { render :new }
+       render :new 
 
       end
-    end
   end
+  
 
   def update
 
       if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
+        redirect_to @review, notice: 'Review was successfully updated.' 
 
       else
-        format.html { render :edit }
+        render :edit 
 
 
     end
   end
+
+  def destroy
+    @review.destroy
+    redirect_to reviews_url
+  end
+
+  private
+  #Use callbacks to share common setup or constrains between actions.
+
+  def set_review
+    @review = Review.find(param[:id])
+  end
+
+  def correct_user
+    @review = current_user.reviews.find_by(id: params[:id])
+    redirect_to pins_path, notice: "Not authorized to edit this review." if @review.nil?
+  end
+
+  def pin_params
+    params.require(:pin).permit(:description, :image)
+  end
+  
+end
 
 
   def destroy
@@ -61,4 +84,4 @@ class ReviewsController < ApplicationController
     def review_params
       params.require(:review).permit(:description, :string)
     end
-    
+

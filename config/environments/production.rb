05f4216955279e2e7d8004c7a1f9fbbae4bca4f3
joycehliting <joycehliting@gmail.com>
2015-02-02 10:40:56 +0800

    Added Amazon s3 for Paperclip uploads to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,15 @@ Rails.application.configure do
   #Required for devise
   config.action_mailer.default_url_options = { :host => 'https://bentobooks.herokuapp.com/' }
 
+  #Let's paperclip upload files to S3
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
 
 end
